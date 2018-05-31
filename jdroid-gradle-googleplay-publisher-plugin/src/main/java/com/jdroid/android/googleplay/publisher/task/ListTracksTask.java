f18ccbc150fb33504b28a01e5274d214b25be012
Maxi Rosson <maxirosson@gmail.com>
2018-05-30 22:39:05 -0300

    Fixed ListTracksTask

--- jdroid-gradle-googleplay-publisher-plugin/src/main/java/com/jdroid/android/googleplay/publisher/task/ListTracksTask.java
+++ jdroid-gradle-googleplay-publisher-plugin/src/main/java/com/jdroid/android/googleplay/publisher/task/ListTracksTask.java
@@ -1,6 +1,7 @@
 package com.jdroid.android.googleplay.publisher.task;
 
 import com.google.api.services.androidpublisher.model.Track;
+import com.google.api.services.androidpublisher.model.TrackRelease;
 import com.google.api.services.androidpublisher.model.TracksListResponse;
 import com.jdroid.android.googleplay.publisher.App;
 import com.jdroid.android.googleplay.publisher.GooglePlayPublisher;
@@ -10,18 +11,24 @@ import org.gradle.api.logging.LogLevel;
 public class ListTracksTask extends BaseTask {
 	
 	public ListTracksTask() {
-		setDescription("List all the assigned APKs for each release track");
+		setDescription("List all the tracks and its releases");
 	}
 
 	@Override
 	protected void onExecute(App app) {
 		TracksListResponse tracksListResponse = GooglePlayPublisher.getTracks(app);
-		getLogger().log(LogLevel.INFO, "-------------------------------");
+		
 		for (Track track : tracksListResponse.getTracks()) {
 			getLogger().log(LogLevel.INFO, "Track: " + track.getTrack());
-			getLogger().log(LogLevel.INFO, "Version codes: " + track.getVersionCodes());
-			getLogger().log(LogLevel.INFO, "User fraction: " + track.getUserFraction());
 			getLogger().log(LogLevel.INFO, "-------------------------------");
+			for (TrackRelease trackRelease : track.getReleases()) {
+				getLogger().log(LogLevel.INFO, "  Release name: " + trackRelease.getName());
+				getLogger().log(LogLevel.INFO, "  Version codes: " + trackRelease.getVersionCodes());
+				getLogger().log(LogLevel.INFO, "  User fraction: " + trackRelease.getUserFraction());
+				getLogger().log(LogLevel.INFO, "  Status: " + trackRelease.getStatus());
+				getLogger().log(LogLevel.INFO, "  Release Notes: " + trackRelease.getReleaseNotes());
+			}
+			getLogger().log(LogLevel.INFO, "/n");
 		}
 	}
 
