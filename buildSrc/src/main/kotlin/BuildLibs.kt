1f11695ec14cce79e436ef541557ef35c46deeca
Maxi Rosson <maxirosson@gmail.com>
2019-09-07 14:28:48 -0300

    Migrated to buiildSrc

---
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -0,0 +1,9 @@
+object BuildLibs {
+    // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
+    const val jdroid_gradle_root_plugin = "com.jdroidtools:jdroid-gradle-root-plugin:2.2.0"
+    const val jdroid_gradle_java_plugin = "com.jdroidtools:jdroid-gradle-java-plugin:2.2.0"
+    const val jdroid_gradle_project_plugin = "com.jdroidtools:jdroid-gradle-project-plugin:2.2.0"
+
+    // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
+    const val jdroid_component_builder = "com.jdroidtools:jdroid-component-builder:2.2.0"
+}
---
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -0,0 +1,9 @@
+object Libs {
+    const val jdroid_java_core = "com.jdroidtools:jdroid-java-core:3.0.0"
+
+    // https://developers.google.com/api-client-library/java/apis/androidpublisher/v3
+    // http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22com.google.apis%22%20AND%20a%3A%22google-api-services-androidpublisher%22
+    const val android_publisher = "com.google.apis:google-api-services-androidpublisher:v3-rev95-1.25.0"
+
+    const val junit = "junit:junit:4.12"
+}
