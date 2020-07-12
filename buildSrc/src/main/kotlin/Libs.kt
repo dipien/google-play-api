334714ab97877d127949f958dbc79fec026db24a
jdroid-ci <jdroidtools@gmail.com>
2020-07-12 00:01:53 +0000

    Upgraded com.google.apis:google-api-services-androidpublisher from v3-rev20200616-1.30.9 to v3-rev20200701-1.30.9

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -3,7 +3,7 @@ object Libs {
 
     // https://developers.google.com/api-client-library/java/apis/androidpublisher/v3
     // http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22com.google.apis%22%20AND%20a%3A%22google-api-services-androidpublisher%22
-    const val ANDROID_PUBLISHER = "com.google.apis:google-api-services-androidpublisher:v3-rev20200616-1.30.9"
+    const val ANDROID_PUBLISHER = "com.google.apis:google-api-services-androidpublisher:v3-rev20200701-1.30.9"
 
     const val JUNIT = "junit:junit:4.13"
 }
