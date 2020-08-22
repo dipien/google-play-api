c686f8763b35cb498f0e857b71c3d777d0c258ff
Maxi Rosson <maxirosson@gmail.com>
2020-08-22 19:46:18 -0300

    Support parameters on reviews and voided purchases services

--- jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/ReviewsService.kt
+++ jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/ReviewsService.kt
@@ -4,8 +4,12 @@ import com.google.api.services.androidpublisher.model.Review
 
 class ReviewsService : AbstractGooglePlayPublisher() {
 
-    fun getReviews(app: App): List<Review> {
-        val listResponse = init(app.appContext).reviews().list(app.applicationId).execute()
-        return listResponse.reviews
+    fun getReviews(app: App, maxResults: Long? = null, paginationToken: String? = null, startIndex: Long? = null, translationLanguage: String? = null): List<Review> {
+        val list = init(app.appContext).reviews().list(app.applicationId)
+        list.maxResults = maxResults
+        list.token = paginationToken
+        list.startIndex = startIndex
+        list.translationLanguage = translationLanguage
+        return list.execute().reviews
     }
 }
--- jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/VoidedPurchasesService.kt
+++ jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/VoidedPurchasesService.kt
@@ -4,8 +4,14 @@ import com.google.api.services.androidpublisher.model.VoidedPurchase
 
 class VoidedPurchasesService : AbstractGooglePlayPublisher() {
 
-    fun getVoidedPurchases(app: App): List<VoidedPurchase> {
-        val listResponse = init(app.appContext).purchases().voidedpurchases().list(app.applicationId).execute()
-        return listResponse.voidedPurchases
+    fun getVoidedPurchases(app: App, maxResults: Long? = null, paginationToken: String? = null, startIndex: Long? = null, startTime: Long? = null, endTime: Long? = null, type: Int? = null): List<VoidedPurchase> {
+        val list = init(app.appContext).purchases().voidedpurchases().list(app.applicationId)
+        list.maxResults = maxResults
+        list.token = paginationToken
+        list.startIndex = startIndex
+        list.startTime = startTime
+        list.endTime = endTime
+        list.type = type
+        return list.execute().voidedPurchases
     }
 }
