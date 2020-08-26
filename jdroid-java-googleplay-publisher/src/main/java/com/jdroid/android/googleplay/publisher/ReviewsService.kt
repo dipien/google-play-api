2566c382d6babb769d73c00d7fc89c386d090d05
Maxi Rosson <maxirosson@gmail.com>
2020-08-26 11:50:01 -0300

    New getFilteredReviews function

--- jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/ReviewsService.kt
+++ jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/ReviewsService.kt
@@ -1,15 +1,47 @@
 package com.jdroid.android.googleplay.publisher
 
 import com.google.api.services.androidpublisher.model.Review
+import com.google.api.services.androidpublisher.model.ReviewsListResponse
 
 class ReviewsService : AbstractGooglePlayPublisher() {
 
-    fun getReviews(app: App, maxResults: Long? = null, paginationToken: String? = null, startIndex: Long? = null, translationLanguage: String? = null): List<Review> {
+    fun getReviews(app: App, maxResults: Long? = null, paginationToken: String? = null, startIndex: Long? = null, translationLanguage: String? = null): ReviewsListResponse {
         val list = init(app.appContext).reviews().list(app.applicationId)
         list.maxResults = maxResults
         list.token = paginationToken
         list.startIndex = startIndex
         list.translationLanguage = translationLanguage
-        return list.execute().reviews
+        return list.execute()
+    }
+
+    fun getFilteredReviews(app: App, appVersionCode: Int? = null, maxRating: Int = 5, translationLanguage: String? = null): List<Review> {
+        return internalGetFilteredReviews(app, appVersionCode, maxRating, null, null, null, translationLanguage)
+    }
+
+    private fun internalGetFilteredReviews(app: App, appVersionCode: Int?, maxRating: Int, maxResults: Long?, paginationToken: String?, startIndex: Long?, translationLanguage: String?): List<Review> {
+        val reviews = mutableListOf<Review>()
+        val reviewsListResponse = getReviews(app, maxResults, paginationToken, startIndex, translationLanguage)
+        reviewsListResponse.reviews.forEach {
+            it.comments.forEach { comment ->
+                if (comment.userComment != null) {
+                    if (appVersionCode == null || comment.userComment.appVersionCode == appVersionCode) {
+                        if (comment.userComment.starRating <= maxRating) {
+                            if (!reviews.contains(it)) {
+                                reviews.add(it)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // TODO Pagination is not working as expected
+        if (reviewsListResponse.pageInfo != null) {
+            if (reviewsListResponse.pageInfo.startIndex + reviewsListResponse.pageInfo.resultPerPage < reviewsListResponse.pageInfo.totalResults) {
+                reviews.addAll(internalGetFilteredReviews(app, appVersionCode, maxRating, maxResults, reviewsListResponse.tokenPagination.nextPageToken,
+                    reviewsListResponse.pageInfo.startIndex.toLong() + reviewsListResponse.pageInfo.resultPerPage, translationLanguage))
+            }
+        }
+
+        return reviews
     }
 }
--- jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/VoidedPurchasesService.kt
+++ jdroid-java-googleplay-publisher/src/main/java/com/jdroid/android/googleplay/publisher/VoidedPurchasesService.kt
@@ -1,10 +1,10 @@
 package com.jdroid.android.googleplay.publisher
 
-import com.google.api.services.androidpublisher.model.VoidedPurchase
+import com.google.api.services.androidpublisher.model.VoidedPurchasesListResponse
 
 class VoidedPurchasesService : AbstractGooglePlayPublisher() {
 
-    fun getVoidedPurchases(app: App, maxResults: Long? = null, paginationToken: String? = null, startIndex: Long? = null, startTime: Long? = null, endTime: Long? = null, type: Int? = null): List<VoidedPurchase> {
+    fun getVoidedPurchases(app: App, maxResults: Long? = null, paginationToken: String? = null, startIndex: Long? = null, startTime: Long? = null, endTime: Long? = null, type: Int? = null): VoidedPurchasesListResponse {
         val list = init(app.appContext).purchases().voidedpurchases().list(app.applicationId)
         list.maxResults = maxResults
         list.token = paginationToken
@@ -12,6 +12,6 @@ class VoidedPurchasesService : AbstractGooglePlayPublisher() {
         list.startTime = startTime
         list.endTime = endTime
         list.type = type
-        return list.execute().voidedPurchases
+        return list.execute()
     }
 }
