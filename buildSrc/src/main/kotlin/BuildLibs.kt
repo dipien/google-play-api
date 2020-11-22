a280a4fde807f94b8e3baad9c10d7df9f47c1983
Jdroid CI <jdroidtools@gmail.com>
2020-11-21 22:46:17 -0300

    Upgraded dependencies for groupId com.jdroidtools (#61)
    * Upgraded com.jdroidtools:jdroid-component-builder from 2.3.0-SNAPSHOT to 2.3.0
    
    * Upgraded com.jdroidtools:jdroid-gradle-java-plugin from 2.4.0-SNAPSHOT to 2.4.0
    
    * Upgraded com.jdroidtools:jdroid-gradle-project-plugin from 2.4.0-SNAPSHOT to 2.4.0
    
    * Upgraded com.jdroidtools:jdroid-gradle-root-plugin from 2.4.0-SNAPSHOT to 2.4.0

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -1,9 +1,9 @@
 object BuildLibs {
     // https://github.com/maxirosson/jdroid-gradle-plugin/blob/master/CHANGELOG.md
-    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0-SNAPSHOT"
-    const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0-SNAPSHOT"
-    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.4.0-SNAPSHOT"
+    const val JDROID_GRADLE_ROOT_PLUGIN = "com.jdroidtools:jdroid-gradle-root-plugin:2.4.0"
+    const val JDROID_GRADLE_JAVA_PLUGIN = "com.jdroidtools:jdroid-gradle-java-plugin:2.4.0"
+    const val JDROID_GRADLE_PROJECT_PLUGIN = "com.jdroidtools:jdroid-gradle-project-plugin:2.4.0"
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
-    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0-SNAPSHOT"
+    const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0"
 }
