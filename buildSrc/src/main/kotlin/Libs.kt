dac56e17352cc57e16e1857edee09a1f31c233af
jdroid-ci <jdroidtools@gmail.com>
2021-02-14 00:02:06 +0000

    Upgraded junit:junit from 4.13.1 to 4.13.2

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -5,5 +5,5 @@ object Libs {
     // http://search.maven.org/#search%7Cgav%7C1%7Cg%3A%22com.google.apis%22%20AND%20a%3A%22google-api-services-androidpublisher%22
     const val ANDROID_PUBLISHER = "com.google.apis:google-api-services-androidpublisher:v3-rev142-1.25.0"
 
-    const val JUNIT = "junit:junit:4.13.1"
+    const val JUNIT = "junit:junit:4.13.2"
 }
