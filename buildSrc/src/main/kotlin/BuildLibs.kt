48c33e7b3eaf4fd199bfeb31de6b288635b9b764
Maxi Rosson <maxirosson@gmail.com>
2021-03-27 23:34:02 -0300

    Update BuildLibs.kt

--- buildSrc/src/main/kotlin/BuildLibs.kt
+++ buildSrc/src/main/kotlin/BuildLibs.kt
@@ -6,4 +6,7 @@ object BuildLibs {
 
     // https://github.com/maxirosson/jdroid-component-builder/blob/master/CHANGELOG.md
     const val JDROID_COMPONENT_BUILDER = "com.jdroidtools:jdroid-component-builder:2.3.0"
+    
+    // TODO Remove this once this prokject is migrated to jdroid gradle plugins v2.5.0
+    const val RELEASES_HUB_GRADLE_PLUGIN = "com.dipien:releases-hub-gradle-plugin:2.0.2"
 }
