0c5843333ed2041cd5bf03db47b78cff6797cf44
Maxi Rosson <maxirosson@gmail.com>
2021-05-11 16:33:16 -0300

    Rename projects

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ plugins {
     id("com.gradle.enterprise").version("3.1.1")
 }
 
-include(":jdroid-gradle-googleplay-publisher-plugin")
-include(":jdroid-java-googleplay-publisher")
+include(":googleplay-api-plugin")
+include(":googleplay-api-kotlin")
